42baeff2b3e68e4f9c1fe96d3ba57321f8e1b788
sorakim <sora6418@gmail.com>
2024-05-29 12:49:54 +0900

    feat: 레이아웃 컴포넌트 추가

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -1,17 +1,16 @@
 import styled from "@emotion/styled";
 
-const Layout = ({children}) => {
-  
-  return (
-    <Container>
-        {children}
-    </Container>
-  )
-}
+const Layout = ({ children }) => {
+  return <Container>{children}</Container>;
+};
 
 const Container = styled.div`
-  
+  display: flex;
+  flex-direction: column;
+  gap: 60px;
+  margin: 0 auto;
+  max-width: 1280px;
+  padding: 60px;
 `;
 
-
-export default Layout
+export default Layout;
